ad5613aa05cc00ee3c2899fa4e9b896f5578880f
Joie Ng <jn437@cornell.edu>
2019-11-21 15:59:39 -0500

    starter files

---
+++ build/precache-manifest.0513e072f06204ff84396f32c68d4952.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf0df5e377229737fc53b1b31e87dca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "623cc570b6774c43af8e",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "7e33f6af042ad543b877",
+    "url": "/static/js/2.3bf2fec7.chunk.js"
+  },
+  {
+    "revision": "623cc570b6774c43af8e",
+    "url": "/static/js/main.62c547b8.chunk.js"
+  },
+  {
+    "revision": "f9aba692f8ee38039645",
+    "url": "/static/js/runtime-main.00b0ec37.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
